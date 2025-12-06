9bb386309ba1dfd22ededd9d82d6e3074a53e018
恒成立 <wo51study@163.com>
2025-12-06 13:04:10 +0800

    1.构建一个静态网页

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const port = 3000
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
     const filePath = path.join(__dirname, 'public', 'home.html');
